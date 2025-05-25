012b606b645919adb4e5cfbfbe200d640d240aa6
Sathwik-Goloju <sathwik.goloju@gmail.com>
2025-05-26 00:11:36 +0530

    Added controller and jdbc connection in properties

---
+++ dbapi/src/main/java/com/bms/dbapi/controller/User.java
@@ -0,0 +1,11 @@
+package com.bms.dbapi.controller;
+
+import com.bms.dbapi.models.AppUser;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class User {
+
+    public void createUser(@RequestBody AppUser appUser){
+
+    }
+}
